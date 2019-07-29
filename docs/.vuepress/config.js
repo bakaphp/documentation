96ca714d60a5048d828671ef6ce45b987baaa939
Danny Feliz <dannyfeliz08@gmail.com>
2019-07-29 17:12:21 -0400

    Fix ignore

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: "Baka Documentation",
+  description: 'Just playing around',
+  themeConfig: {
+    logo: 'assets/img/logo.svg',
+    lastUpdated: 'Last Updated',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Rapid Crud', link: '/rapid-crud/' },
+      { text: 'Example Feature', link: '/example-feature/' }
+    ],
+    displayAllHeaders: true,
+    sidebar: 'auto',
+    serviceWorker: {
+      updatePopup: true
+    },
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: 'Help us improve this page!',
+    repo: 'bakaphp/documentation',
+  }
+}
